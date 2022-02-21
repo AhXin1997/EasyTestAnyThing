9bcf0d0e6c439fd09b48b5ec41b6f927cd50d7df
AhXin1997 <ghostwater1997@gmail.com>
2022-02-21 17:15:24 +0800

    Add TODO List

--- EasyTestAnyThing/Program.cs
+++ EasyTestAnyThing/Program.cs
@@ -8,5 +8,32 @@ namespace EasyTestAnyThing
         {
             Console.ReadKey();
         }
+
+        private static void TODO_List() 
+        {
+            /*
+            Add Tuple       C# Example
+            Add Thread Safe C# Example
+            Add Queue       C# Example
+            Flurl 套件熟悉(串Url)
+
+            Add UnitTestsThing > Program.cs
+            Add UnitTestsThing > MSClass > .cs File
+            Add UnitTestsThing > UnitTestThingTests > MSClass > .cs File
+
+            Consolo 導入 Mvc架構 可由 PostMan 打入Api
+            Add Async       Example
+            Add Repository  Example
+            Add Linq        Example
+            Add Fliter      Example
+
+            Try Add Db
+            Mapping 
+                one to one
+                one to many
+                many to many
+
+             */
+        }
     }
 }
