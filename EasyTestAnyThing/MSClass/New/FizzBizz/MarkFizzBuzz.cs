caca7bd59275bc765da7001afa3a08592d683762
AhXin <ghostwater1997@gmail.com>
2022-03-06 16:06:03 +0800

    Update FizzBuzz

--- EasyTestAnyThing/MSClass/New/FizzBizz/MarkFizzBuzz.cs
+++ EasyTestAnyThing/MSClass/New/FizzBizz/MarkFizzBuzz.cs
@@ -4,85 +4,53 @@ using System.Linq;
 
 namespace EasyTestAnyThing.MSClass.New.FizzBizz
 {
-    /*
-     * 3的倍數印Fizz、5的倍數印Buzz
-     */
-
     public class MarkFizzBuzz
     {
+        /// <summary>
+        /// 3的倍數印Fizz、5的倍數印Buzz
+        /// </summary>
         public void StartMethod()
         {
-            var x = new List<int>();
-            for (int i = 0; i < 101; i++)
-            {
-                x.Add(i);
-            };
-            var w = MarkFizzBuzzMethod01(x);
-            w.ForEach(f => Console.WriteLine(f));
+            MarkFizzBuzzMethod(new List<int>(Enumerable.Range(0, 101)))
+                .ForEach(f => Console.WriteLine(f));
         }
 
-        public List<string> MarkFizzBuzzMethod(List<int> list)
+        public List<string> MarkFizzBuzzMethod(IEnumerable<int> list)
         {
-            var str = string.Empty;
-            foreach (var item in list)
-            {
-                str += item + " ";
-                if (item % 3 == 0) str += "Fizz";
-                if (item % 5 == 0) str += "Buzz";
-                if (item % 7 == 0) str += "Hot";
-                str += ",";
-            }
-            return new List<string>
-                (
-                    str.Remove(str.Length - 1, 1)
-                    .Split(',')
-                    .Select(s => s.Trim())
-                    .ToList()
-                ); ;
-        }
-
-        public Dictionary<int, string> keyValuePairs = new Dictionary<int, string> 
-        {
-            {3,"Fizz"},
-            {5,"Buzz"},
-            {15,"Hot"},
-            {7,"Omg"},
-        };
-
-        public List<string> MarkFizzBuzzMethod01(List<int> list)
-        {
-            var str = string.Empty;
-            var x = new List<string>();
+            var outputStr = string.Empty;
             foreach (var num in list)
             {
-                foreach (var item in keyValuePairs)
-                {
-                    if (num % item.Key == 0)
-                    {
-                        x.Add(item.Value);
-                    }
-                    else
-                    {
-                        x.Add("");
-                    }
-                }
-
-                str += num;
-                foreach (var ggg in x)
-                {
-                    str += ggg;
-                }
-                str += ",";
-
-                x.Clear();
+                var valueWords = new List<string>();
+                valueWords.AddRange(
+                    KeyValuePairs
+                        .Where(s => num % s.Key == 0)
+                        .Select(w => w.Value)
+                );
+                var str = string.Join(string.Empty, valueWords);
+                outputStr += string.IsNullOrWhiteSpace(str) ?
+                            $"{num}," :
+                            $"{num} - {str},";
             }
 
             return new List<string>
                 (
-                    str.Remove(str.Length - 1, 1)
+                    outputStr.Remove(outputStr.Length - 1, 1)
                     .Split(',')
                     .ToList()
                 );
         }
+
+        protected virtual IDictionary<int, string> KeyValuePairs
+        {
+            get => _keyValuePairs;
+        }
+
+        private readonly Dictionary<int, string> _keyValuePairs = new Dictionary<int, string>
+        {
+            {3,"Fizz"},
+            {5,"Buzz"},
+            {7,"Moly"},
+            {15,"Hot"},
+        };
     }
 }
--- EasyTestAnyThingTest/MSClass/New/FizzBizz/MarkFizzBuzzTests.cs
+++ EasyTestAnyThingTest/MSClass/New/FizzBizz/MarkFizzBuzzTests.cs
@@ -1,28 +1,99 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Xunit;
+﻿using EasyTestAnyThing.MSClass.New.FizzBizz;
 using FluentAssertions;
-using EasyTestAnyThing.MSClass.New.FizzBizz;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
 
 namespace EasyTestAnyThingTest.MSClass.New.FizzBizz
 {
+    public class FakeMarkFizzBuzz : MarkFizzBuzz
+    {
+        protected override IDictionary<int, string> KeyValuePairs
+        {
+            get => _keyValuePairs;
+        }
+
+        public Dictionary<int, string> _keyValuePairs =
+            new Dictionary<int, string> { };
+    }
+
     public class MarkFizzBuzzTests
     {
-        private readonly MarkFizzBuzz _target;
+        private readonly FakeMarkFizzBuzz _target;
 
-        public MarkFizzBuzzTests() 
+        public MarkFizzBuzzTests()
         {
-            _target = new MarkFizzBuzz();
+            _target = new FakeMarkFizzBuzz();
         }
+
         [Fact]
-        public void test() 
+        public void MarK_Fizz_When_Num_Can_Be_Divided_Exactly_By_3()
         {
-            var list = new List<int> { 1, 2, 3, 4, 5,15};
-            _target.MarkFizzBuzzMethod(list).Should().BeEquivalentTo(new List<string> 
-            { 
-                "1","2","3 Fizz","4","5 Buzz","15 FizzBuzz"
-            });
+            _target._keyValuePairs.Add(3, "Fizz");
+
+            _target.MarkFizzBuzzMethod(Enumerable.Range(0, 11))
+                .Should()
+                .BeEquivalentTo(new List<string>
+                {
+                    "0 - Fizz",
+                    "1",
+                    "2",
+                    "3 - Fizz",
+                    "4",
+                    "5",
+                    "6 - Fizz",
+                    "7",
+                    "8",
+                    "9 - Fizz",
+                    "10"
+                });
+        }
+
+        [Fact]
+        public void MarK_Buzz_When_Num_Can_Be_Divided_Exactly_By_5()
+        {
+            _target._keyValuePairs.Add(5, "Buzz");
+
+            _target.MarkFizzBuzzMethod(Enumerable.Range(0, 11))
+                .Should()
+                .BeEquivalentTo(new List<string>
+                {
+                    "0 - Buzz",
+                    "1",
+                    "2",
+                    "3",
+                    "4",
+                    "5 - Buzz",
+                    "6",
+                    "7",
+                    "8",
+                    "9",
+                    "10 - Buzz"
+                });
+        }
+
+        [Fact]
+        public void MarK_FizzBuzz_When_Num_Can_Be_Divided_Exactly_By_3_And_5()
+        {
+            _target._keyValuePairs.Add(3, "Fizz");
+            _target._keyValuePairs.Add(5, "Buzz");
+
+            _target.MarkFizzBuzzMethod(Enumerable.Range(0, 16))[15]
+                .Should().Be("15 - FizzBuzz");
+            //.BeEquivalentTo(new List<string>
+            //{
+            //    "0 - FizzBuzz",
+            //    "1",
+            //    "2",
+            //    "3 - Fizz",
+            //    "4",
+            //    "5 - Buzz",
+            //    "6 - Fizz",
+            //    "7",
+            //    "8",
+            //    "9 - Fizz",
+            //    "10 - Buzz"
+            //});
         }
     }
 }
