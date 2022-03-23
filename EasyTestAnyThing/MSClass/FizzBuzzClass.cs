520c50eeaff5c3f45975eca9f82761ea67039632
AhXin <ghostwater1997@gmail.com>
2022-03-23 21:56:38 +0800

    Add New Reverse Word Class

--- EasyTestAnyThing/MSClass/FizzBuzzClass.cs
+++ EasyTestAnyThing/MSClass/FizzBuzzClass.cs
@@ -6,35 +6,23 @@ namespace EasyTestAnyThing.MSClass
 {
     public class FizzBuzzClass
     {
+        /*
+            2022/01/05
+            使用 C# 的 for 陳述式逐一查看程式碼區塊
+
+            輸出 1 到 100 的值，每行一個數字。
+            當目前的值可由 3 整除時，在該數字旁邊列印 Fizz 字詞。
+            當目前的值可由 5 整除時，在該數字旁邊列印 Buzz 字詞。
+            當目前的值可由 3 和 5 同時 整除時，在該數字旁邊列印 FizzBuzz 字詞。
+       */
+
         public IEnumerable<string> Run(int count)
         {
 
-            //var test = Enumerable.Range(1, count)
-            //    .Select(i => Logic(i))
-            //    .Select((item, index) => Display(item, index));
-            //return test.ToList();
-
-            var result = new List<string>();
-            for (int i = 1; i <= count; i++)
-            {
-                if (i % 15 == 0)
-                {
-                    result.Add(i + " - " + "FizzBuzz");
-                }
-                else if (i % 3 == 0)
-                {
-                    result.Add(i + " - " + "Fizz");
-                }
-                else if (i % 5 == 0)
-                {
-                    result.Add(i + " - " + "Buzz");
-                }
-                else
-                {
-                    result.Add(i + " - " + "");
-                }
-            }
-            return result;
+            var test = Enumerable.Range(1, count)
+                .Select(i => Logic(i))
+                .Select((item, index) => Display(item, index));
+            return test.ToList();
         }
         private static string Display(string item, int index)
         {
---
+++ EasyTestAnyThing/MSClass/New/CharReverse/NewCharReverse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace EasyTestAnyThing.MSClass.New.CharReverse
+{
+    public class NewCharReverse
+    {
+        /*
+         * 使用 C# 中的協助程式方法在陣列上執行作業
+         * The quick brown fox jumps over the lazy dog
+         * To
+         * ehT kciuq nworb xof spmuj revo eht yzal god
+         */
+
+        public string ReverseLogic(string str,char splitWith)
+        {
+            return string.Join(
+                " ", 
+                str.Split(splitWith).Select(s => string.Join(string.Empty, s.Reverse())));
+        }
+    }
+}
