76056be6444c778a1f103540b0ee7801dd059fe8
AhXin <ghostwater1997@gmail.com>
2022-03-23 22:38:55 +0800

    Add NewLocker V1 Ask Why

---
+++ EasyTestAnyThing/MSClass/New/Locker/NewLocker.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EasyTestAnyThing.MSClass.New.Locker
+{
+    public class NewLocker
+    {
+        private static object _locker => new object();
+
+        public void SameTimeInserSomeThing()
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                Task.Run(() => InserSomeThing(i));
+            }
+        }
+
+        private void InserSomeThing(decimal amount)
+        {
+            lock (_locker)
+            {
+                Wallet.Blance += amount;
+                Console.WriteLine(Thread.CurrentThread.ManagedThreadId + ",Now Blance is " + Wallet.Blance);
+            }
+        }
+    }
+
+    public static class Wallet
+    {
+        public static decimal Blance { get; set; }
+    }
+}
--- EasyTestAnyThing/Program.cs
+++ EasyTestAnyThing/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using EasyTestAnyThing.MSClass.New.Locker;
+using System;
+using System.Threading.Tasks;
 
 namespace EasyTestAnyThing
 {
@@ -6,6 +8,18 @@ namespace EasyTestAnyThing
     {
         private static void Main()
         {
+            var x = new NewLocker();
+            x.SameTimeInserSomeThing();
+            Console.WriteLine(Wallet.Blance);
+
+            var shouldBe = 0;
+            for (int i = 1; i < 10; i++)
+            {
+                shouldBe += i;
+            }
+            Console.WriteLine(shouldBe);
+
+
             //Tool
             //var tool = new EasyOutputMessage();
             //tool.EasyOutputMessageMethod();
@@ -17,18 +31,18 @@ namespace EasyTestAnyThing
          * Add Thread Safe C# Example
          * Learn How to use Fun Action
          * Flurl 套件熟悉(串Url)
-         * 
+         *
          * [✓] Console 導入 Mvc架構 可由 PostMan 打入Api
          * Add Filter      Example
-         * 
+         *
          * Try Add Db
          * Add Repository  Example
          * Mapping
          *     one to one
          *     one to many
          *     many to many
-         * 
-         * 
+         *
+         *
          */
     }
 }
