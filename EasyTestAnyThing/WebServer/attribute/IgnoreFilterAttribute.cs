4a2a7153433d138ef150748cf8927a1b30a97810
AhXin <ghostwater1997@gmail.com>
2022-04-04 16:33:37 +0800

    Add WebServer Filter

---
+++ EasyTestAnyThing/WebServer/attribute/IgnoreFilterAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EasyTestAnyThing.WebServer.attribute
+{
+    public class IgnoreFilterAttribute : Attribute
+    {
+        public Type FilterType { get; }
+
+        public IgnoreFilterAttribute(Type filterType)
+        {
+            FilterType = filterType;
+        }
+    }
+}
